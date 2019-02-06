ceb762d8da6a94a9a9e64fe9908d250b8eb9f3be
cm-hirata.kei <hirata.kei@classmethod.jp>
2019-02-07 05:25:20 +0900

    layer作成のためのスクリプトを追加

---
+++ layer/make.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir -p python/bin/
+
+# download headless-chrome
+if [ ! -f python/bin/headless-chromium ]; then
+    curl -SL https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-37/stable-headless-chromium-amazonlinux-2017-03.zip > headless-chromium.zip
+    unzip headless-chromium.zip -d python/bin/
+    rm headless-chromium.zip
+fi
+
+# download webdriver
+if [ ! -f python/bin/chromedriver ]; then
+    curl -SL https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip > chromedriver.zip
+    unzip chromedriver.zip -d python/bin/
+    rm chromedriver.zip
+fi
+
+# make layer package
+if [ ! -f layer.zip ]; then
+    zip -r9 layer.zip python/
+fi
